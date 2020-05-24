a92ab6a96831b921fa87efcb9d4e58519d01f331
TakaoWing <tt.wing2838783@gmail.com>
2020-05-24 09:31:35 +0900

    add RaycastController

---
+++ Assets/Scripts/ReyCastController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReyCastController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = 100; // 飛ばす&表示するRayの長さ
+        float duration = 0f;   // 表示期間（秒）
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.red, duration, false);
+
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(ray, out hit, distance)) {
+            GameObject hitObject = hit.collider.gameObject;
+            print(hitObject.name);
+            // （以下略）
+        }
+    }
+}
